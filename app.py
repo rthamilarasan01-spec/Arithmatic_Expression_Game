aaa79fbc5fa6e8ac37671d1645959d57b6f46cee
Thamilarasu <rthamilarasan01@gmail.com>
2026-06-21 18:22:22 +0530

    Database Update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres.iuduoczxcftbncfciktb:[YOUR-PASSWORD]@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 print("DATABASE_URL =", os.environ.get("DATABASE_URL"))
